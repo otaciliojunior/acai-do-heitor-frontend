90af46113fa6b30472530c531cb2f82c93ee0783
otaciliojunior <otaciliojr.quirino@gmail.com>
2025-08-17 11:10:44 -0300

    configura a URL da API de produção

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM carregado. Iniciando script...");
 
     // --- 1. CONFIGURAÇÕES ---
-    const API_URL = 'http://localhost:3000'; // <-- CORREÇÃO
+    const API_URL = 'https://acai-do-heitor-backend.onrender.com';
     const WHATSAPP_NUMBER = "5584981475289";
     const DELIVERY_FEE = 2.00;
 
